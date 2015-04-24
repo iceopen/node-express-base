c97e2ad9e5c32010484a729000d564a4107f9b9e
iceinto <iceinto@163.com>
2015-04-24 11:31:05 +0800

    添加优化 可以支持变量进行端口设置

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var config = require('./config');
 var logger = require("./util/Log4jsUtil");
 var apis = require('./apis');
 var async = require("async");
-
+var port = (process.env.VMC_APP_PORT || config.port);
+var host = (process.env.VCAP_APP_HOST || 'localhost');
 if (!config.debug) {
     require('newrelic');
 }
@@ -12,9 +13,9 @@ app.use(log4js.connectLogger(log4js.getLogger("http"), {level: 'auto'}));
 
 app.get("*", function (req, res) {
     res.send('Hell');
-})
+});
 
-var server = app.listen(config.port, function (req, res) {
+var server = app.listen(port, function (req, res) {
     logger.info('Listening on port ' + server.address().port);
 });
 
